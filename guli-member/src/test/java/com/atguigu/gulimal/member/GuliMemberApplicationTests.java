7c73cf789115810b340e955fe02316ce5ca94a7d
TerryFan <fyx592768036@gmail.com>
2023-07-24 23:32:50 +0800

    项目初始结构创建

---
+++ guli-member/src/test/java/com/atguigu/gulimal/member/GuliMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimal.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
